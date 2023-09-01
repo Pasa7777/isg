c5fc31b13f380e9e42ff0a55a9a1dfa3a8cbe507
BuildTools <unconfigured@null.spigotmc.org>
2023-09-01 14:19:35 +0300

    installing isg sh script

---
+++ isg_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt install curl git ffmpeg libopencv-dev clang libclang-dev build-essential libssl-dev -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+git clone https://github.com/DvorakDwarf/Infinite-storage-glitch
+cd Infinite-storage-glitch
+cargo build --release
+exit
