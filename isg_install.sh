0af2f572c129e2ee2e30eab4347f251b9f706cc7
Pasa7777 <106025935+Pasa7777@users.noreply.github.com>
2023-09-02 13:08:33 +0300

    Update isg_install.sh

--- isg_install.sh
+++ isg_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install curl git ffmpeg libopencv-dev clang libclang-dev build-essential libssl-dev -y
+apt update
+apt install curl git ffmpeg libopencv-dev clang libclang-dev build-essential libssl-dev -y
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 git clone https://github.com/DvorakDwarf/Infinite-storage-glitch
 cd Infinite-storage-glitch
